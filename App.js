7f122b5b421653b24aea36c3ed3d50e02645683c
sednanref <v936955@gmail.com>
2024-02-15 15:13:36 -0300

    primeiro commit

---
+++ App.js
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.boasvindas}>Olá Mundo React Native!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  boasvindas: {
+    color: '#568975',
+    backgroundColor: '#DDDDDD',
+    fontSize: 30
+  }
+});
